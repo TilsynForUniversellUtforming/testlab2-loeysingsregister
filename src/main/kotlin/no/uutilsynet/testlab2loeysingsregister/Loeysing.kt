fa798a58414ac47d7cf0d65f5c451ff1f6239d5a
Lars Lønne <lonnelars@gmail.com>
2023-11-02 13:52:32 +0100

    oppdatering av løysingar
    Implementerer oppdatering av løysingar ved å lagre delvise løysingar i databasen, og sette dei saman når dei hentast ut.
    
    DFK-374

--- src/main/kotlin/no/uutilsynet/testlab2loeysingsregister/Loeysing.kt
+++ src/main/kotlin/no/uutilsynet/testlab2loeysingsregister/Loeysing.kt
@@ -5,17 +5,11 @@ import kotlin.reflect.full.memberProperties
 
 data class Loeysing(val id: Int, val namn: String, val url: URL, val orgnummer: String)
 
-data class LoeysingDiff(val id: Int?, val namn: String?, val url: URL?, val orgnummer: String?)
+data class LoeysingDiff(val namn: String?, val url: URL?, val orgnummer: String?)
 
 fun diff(a: Loeysing, b: Loeysing): LoeysingDiff =
     Loeysing::class
         .memberProperties
         .filter { it.get(a).toString() != it.get(b).toString() }
         .associate { it.name to it.get(b) }
-        .let {
-          LoeysingDiff(
-              it["id"] as Int?,
-              it["namn"] as String?,
-              it["url"] as URL?,
-              it["orgnummer"] as String?)
-        }
+        .let { LoeysingDiff(it["namn"] as String?, it["url"] as URL?, it["orgnummer"] as String?) }
