faf5a1d7bcf43ace919962326019e97588b26935
Lars Lønne <lonnelars@gmail.com>
2023-11-02 13:52:32 +0100

    oppdatering av løysingar
    Implementerer oppdatering av løysingar ved å lagre delvise løysingar i databasen, og sette dei saman når dei hentast ut.
    
    DFK-374

--- src/main/kotlin/no/uutilsynet/testlab2loeysingsregister/LoeysingDAO.kt
+++ src/main/kotlin/no/uutilsynet/testlab2loeysingsregister/LoeysingDAO.kt
@@ -1,10 +1,7 @@
 package no.uutilsynet.testlab2loeysingsregister
 
 import java.net.URL
-import no.uutilsynet.testlab2loeysingsregister.LoeysingDAO.LoeysingParams.getLoeysingListSql
-import no.uutilsynet.testlab2loeysingsregister.LoeysingDAO.LoeysingParams.getLoeysingSql
 import no.uutilsynet.testlab2loeysingsregister.LoeysingDAO.LoeysingParams.loeysingRowMapper
-import org.springframework.dao.support.DataAccessUtils
 import org.springframework.jdbc.core.DataClassRowMapper
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Component
@@ -14,17 +11,54 @@ import org.springframework.transaction.annotation.Transactional
 class LoeysingDAO(val jdbcTemplate: NamedParameterJdbcTemplate) {
 
   object LoeysingParams {
-    val getLoeysingListSql = "select id, namn, url, orgnummer from loeysing order by id"
-    val getLoeysingSql = "select id, namn, url, orgnummer from loeysing where id = :id order by id"
 
     val loeysingRowMapper = DataClassRowMapper.newInstance(Loeysing::class.java)
   }
 
-  fun getLoeysing(id: Int): Loeysing? =
-      DataAccessUtils.singleResult(
-          jdbcTemplate.query(getLoeysingSql, mapOf("id" to id), loeysingRowMapper))
+  private data class PartialLoeysing(
+      val id: Int,
+      val namn: String?,
+      val url: String?,
+      val orgnummer: String?,
+      val original: Int
+  )
 
-  fun getLoeysingList(): List<Loeysing> = jdbcTemplate.query(getLoeysingListSql, loeysingRowMapper)
+  private fun combine(base: PartialLoeysing, update: PartialLoeysing): PartialLoeysing =
+      PartialLoeysing(
+          id = base.original,
+          namn = update.namn ?: base.namn,
+          url = update.url ?: base.url,
+          orgnummer = update.orgnummer ?: base.orgnummer,
+          original = update.original)
+
+  fun getLoeysing(id: Int): Loeysing? = getLoeysingList(listOf(id)).firstOrNull()
+
+  fun getLoeysingList(idList: List<Int>? = null): List<Loeysing> {
+    val whereClause: String =
+        if (idList != null) {
+          "where original in (:idList)"
+        } else {
+          ""
+        }
+    val partials =
+        jdbcTemplate.query(
+            """
+              select id, namn, url, orgnummer, original
+              from loeysing
+              $whereClause
+              order by tidspunkt
+          """
+                .trimIndent(),
+            mapOf("idList" to idList),
+            DataClassRowMapper.newInstance(PartialLoeysing::class.java))
+    return partials
+        .groupBy { it.original }
+        .map { (_, partials) ->
+          partials.reduce(::combine).let {
+            Loeysing(it.id, it.namn!!, URL(it.url!!), it.orgnummer!!)
+          }
+        }
+  }
 
   @Transactional
   fun createLoeysing(namn: String, url: URL, orgnummer: String, id: Int? = null): Int =
@@ -62,15 +96,22 @@ class LoeysingDAO(val jdbcTemplate: NamedParameterJdbcTemplate) {
     return sammeOrgnummer.find { loeysing -> sameURL(loeysing.url, url) }
   }
 
-  fun getLoeysingList(idList: List<Int>): List<Loeysing> {
-    return jdbcTemplate.query(
+  @Transactional
+  fun update(updated: Loeysing): Result<Unit> = runCatching {
+    val latest =
+        getLoeysing(updated.id)
+            ?: throw IllegalArgumentException("Fant ikke løysing med id ${updated.id}")
+    val diff = diff(latest, updated)
+    jdbcTemplate.update(
         """
-              select id, namn, url, orgnummer
-              from loeysing
-              where id in (:idList)
-          """
+          insert into loeysing (namn, url, orgnummer, original, tidspunkt)
+          values (:namn, :url, :orgnummer, :original, now())
+        """
             .trimIndent(),
-        mapOf("idList" to idList),
-        loeysingRowMapper)
+        mapOf(
+            "namn" to diff.namn,
+            "url" to diff.url?.toString(),
+            "orgnummer" to diff.orgnummer,
+            "original" to latest.id))
   }
 }
