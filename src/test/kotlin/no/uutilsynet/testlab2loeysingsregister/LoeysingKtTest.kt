dc25102b3ea2742572f0f51e22128aaf7735e4e7
Lars Lønne <lonnelars@gmail.com>
2023-11-02 13:52:32 +0100

    oppdatering av løysingar
    Implementerer oppdatering av løysingar ved å lagre delvise løysingar i databasen, og sette dei saman når dei hentast ut.
    
    DFK-374

--- src/test/kotlin/no/uutilsynet/testlab2loeysingsregister/LoeysingKtTest.kt
+++ src/test/kotlin/no/uutilsynet/testlab2loeysingsregister/LoeysingKtTest.kt
@@ -16,20 +16,7 @@ class LoeysingKtTest {
     fun toLike() {
       val loeysing =
           Loeysing(1, "UUTilsynet", URI("https://www.uutilsynet.no/").toURL(), "991825827")
-      assertThat(diff(loeysing, loeysing)).isEqualTo(LoeysingDiff(null, null, null, null))
-    }
-
-    @DisplayName("når vi differ to ulike løysingar, så skal alle felta vere endra")
-    @Test
-    fun toUlike() {
-      val uutilsynet =
-          Loeysing(1, "UUTilsynet", URI("https://www.uutilsynet.no/").toURL(), "991825827")
-      val askerKommune =
-          Loeysing(2, "Asker kommune", URI("https://www.asker.kommune.no/").toURL(), "920125298")
-      assertThat(diff(uutilsynet, askerKommune))
-          .isEqualTo(
-              LoeysingDiff(
-                  2, "Asker kommune", URI("https://www.asker.kommune.no/").toURL(), "920125298"))
+      assertThat(diff(loeysing, loeysing)).isEqualTo(LoeysingDiff(null, null, null))
     }
 
     @DisplayName("når vi differ samme løysing med ulikt namn, så skal namnet vere endra")
@@ -38,7 +25,7 @@ class LoeysingKtTest {
       val loeysing =
           Loeysing(1, "UUTilsynet", URI("https://www.uutilsynet.no/").toURL(), "991825827")
       val loeysing2 = Loeysing(1, "TUU", URI("https://www.uutilsynet.no/").toURL(), "991825827")
-      assertThat(diff(loeysing, loeysing2)).isEqualTo(LoeysingDiff(null, "TUU", null, null))
+      assertThat(diff(loeysing, loeysing2)).isEqualTo(LoeysingDiff("TUU", null, null))
     }
 
     @DisplayName("når vi differ samme løysing med ulik url, så skal url vere endra")
@@ -48,7 +35,7 @@ class LoeysingKtTest {
           Loeysing(1, "UUTilsynet", URI("https://www.uutilsynet.no/").toURL(), "991825827")
       val loeysing2 = Loeysing(1, "UUTilsynet", URI("https://www.digdir.no/").toURL(), "991825827")
       assertThat(diff(loeysing, loeysing2))
-          .isEqualTo(LoeysingDiff(null, null, URI("https://www.digdir.no/").toURL(), null))
+          .isEqualTo(LoeysingDiff(null, URI("https://www.digdir.no/").toURL(), null))
     }
 
     @DisplayName("når vi differ samme løysing med ulikt orgnummer, så skal orgnummer vere endra")
@@ -58,7 +45,7 @@ class LoeysingKtTest {
           Loeysing(1, "UUTilsynet", URI("https://www.uutilsynet.no/").toURL(), "991825827")
       val loeysing2 =
           Loeysing(1, "UUTilsynet", URI("https://www.uutilsynet.no/").toURL(), "123456785")
-      assertThat(diff(loeysing, loeysing2)).isEqualTo(LoeysingDiff(null, null, null, "123456785"))
+      assertThat(diff(loeysing, loeysing2)).isEqualTo(LoeysingDiff(null, null, "123456785"))
     }
   }
 }
