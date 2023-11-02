6d332626aef7e51a0500628e5d6f4b997b352f10
Lars Lønne <lonnelars@gmail.com>
2023-11-02 11:06:37 +0100

    implementerer diff av to løysingar
    DFK-374

--- src/main/kotlin/no/uutilsynet/testlab2loeysingsregister/Loeysing.kt
+++ src/main/kotlin/no/uutilsynet/testlab2loeysingsregister/Loeysing.kt
@@ -1,7 +1,21 @@
 package no.uutilsynet.testlab2loeysingsregister
 
 import java.net.URL
+import kotlin.reflect.full.memberProperties
 
-data class Loeysing(val id: Int, val namn: String, val url: URL, val orgnummer: String) {
-  data class External(val namn: String, val url: String, val orgnummer: String)
-}
+data class Loeysing(val id: Int, val namn: String, val url: URL, val orgnummer: String)
+
+data class LoeysingDiff(val id: Int?, val namn: String?, val url: URL?, val orgnummer: String?)
+
+fun diff(a: Loeysing, b: Loeysing): LoeysingDiff =
+    Loeysing::class
+        .memberProperties
+        .filter { it.get(a).toString() != it.get(b).toString() }
+        .associate { it.name to it.get(b) }
+        .let {
+          LoeysingDiff(
+              it["id"] as Int?,
+              it["namn"] as String?,
+              it["url"] as URL?,
+              it["orgnummer"] as String?)
+        }
